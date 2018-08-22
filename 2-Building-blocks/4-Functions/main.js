c3e787c2f132d1970dabd92b384f122e79315537
Hendrik Ordoñez B <hendrikob.one@gmail.com>
2018-08-22 11:50:52 -0500

    Functions

---
+++ 2-Building-blocks/4-Functions/main.js
@@ -0,0 +1,17 @@
+var x = 1;
+
+function a() {
+	var y = 2;
+	output(z);
+}
+
+function b() {
+	var z = 3;
+	output(y);
+}
+
+function output(value) {
+	var para = document.createElement('p');
+	document.body.appendChild(para);
+	para.textContent = 'Value: ' + value;
+}
